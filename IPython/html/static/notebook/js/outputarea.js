76205dd89fa4970948a8274dd8aaa6c56fdfda59
Claudine Gladue <gladuec17@macewan.ca>
2014-03-07 00:56:29 -0700

    Add an iframe to output, with a canvas and turtle.

--- IPython/html/static/notebook/js/outputarea.js
+++ IPython/html/static/notebook/js/outputarea.js
@@ -594,22 +594,23 @@ var IPython = (function (IPython) {
         if (extra_class){
             toinsert.addClass(extra_class);
         }
-        
+     
         //output html canvas when "showmetheturtle" is present
-         if (data.search("showmetheturtle") > 0) {
+       /*  if (data.search("showmetheturtle") > 0) {
          	var c = "<canvas id=\"myCanvas\" width=\"400\" height=\"300\" style=\"border:1px solid #000000;\" onmousedown=\"change_coords(event)\"\">";
          	data = data + c;
+         }*/
+        if (data.search("showmetheturtle") > 0) {
+         	var c = "<iframe width=\"500\" height=\"300\" frameborder=\"no\" scrolling=\"no\" marginheight=\"0\"   marginwidth=\"0\" src=\"./IPython/extensions/line.html\"></iframe>"
+         	data = data + c;
          }
-        
         toinsert.append($("<pre/>").html(data));
-        
      	//line count feature, when countthelines present
         if (data.search("countthelines") > 0) {
     		var lines = data.split(/\r\n|\r|\n/);
             var linedata = "Number of Lines: " + (lines.length-1);
         	toinsert.append($("<pre/>").html(linedata));
-        }  
-        
+        }   
         element.append(toinsert);
     };
 

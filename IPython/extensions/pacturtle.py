5692cf1b8a658803b6b07e5a0c08c83435e6fc07
Claudine Gladue <gladuec17@macewan.ca>
2014-03-12 09:23:51 -0600

    pacturtle python libary added.

---
+++ IPython/extensions/pacturtle.py
@@ -0,0 +1,14 @@
+class pacTurtle:
+	print "we made a turtle"
+	
+	def forward( self, x ):
+		#n is the amount we want to move forward
+		print ("forward on!")
+		print x
+	
+	def left( self, x ):
+		#n is the angle we want to rotate left
+		print ("lets go left by some degrees")
+		print x
+	
+	

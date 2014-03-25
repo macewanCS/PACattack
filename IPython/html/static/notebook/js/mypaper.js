51d6948725cdbf22920ef3aceba3f194c7c22113
Claudine Gladue <gladuec17@macewan.ca>
2014-03-25 16:17:43 -0600

    Add backward javascript side.

--- IPython/html/static/notebook/js/mypaper.js
+++ IPython/html/static/notebook/js/mypaper.js
@@ -1,11 +1,7 @@
-
-
-			var d = document.getElementById('canvas1');
-			paper.setup(d);
-						
-			d.style = "border:1px solid #000000;";
-			
-	
+	var d = document.getElementById('canvas1');
+	paper.setup(d);
+	d.style = "border:1px solid #000000;";
+		
 	// Create a raster item using the image tag with id='raster'
 	var raster = new paper.Raster('/static/notebook/js/turtle.png');
 
@@ -25,7 +21,6 @@
 	
 	//set default color
 	var penColor = 'black';
-	
 	
 	// image position
 	raster.position.x = xcoord;
@@ -52,7 +47,7 @@
 	var angle = 90; //this is to keep track of the turtles angle.
 	var newAngle = 0;
 	var getAngle = true;
-
+	var oldAngle = turtleAngle;
 	//Get the events from output area.
 	var myEvents = ($(".myString")).text();
 	myEvents = myEvents.split(" ");			
@@ -122,16 +117,29 @@
     		event.penSize = myEvents[index];
     		eventList.push(event);
     	}
-		
-    	if ( myEvents[index].indexOf('goto') >= 0) {
+    	
+    	if ( myEvents[index].indexOf('backward') >= 0) {
     		var event = [];
     		console.log("goto cmd made");
-    		event.command = "line";
+    		event.command = "backward";
     		++index;
     		event.xend = myEvents[index];
     		++index;
     		event.yend = myEvents[index];		
 	
+    		eventList.push(event);
+    	}
+		
+    	if ( myEvents[index].indexOf('goTo') >= 0) {
+    		var event = [];
+    		console.log("goto cmd made");
+    		event.command = "goTo";
+    		++index;
+    		event.xend = myEvents[index];
+    		++index;
+    		event.yend = myEvents[index];		
+			++index;
+			event.angle = myEvents[index];
     		eventList.push(event);
     	}
     	
@@ -223,9 +231,9 @@
 					var angleRadians = turtleAngle * (Math.PI / 180);
 									
 					//calculate coordinate 
-					yDest = Math.sin(angleRadians).toFixed(2);
+					yDest = Math.sin(angleRadians);
 					yDest = -yDest; //y coord increase in down direction 
-					xDest = Math.cos(angleRadians).toFixed(2);
+					xDest = Math.cos(angleRadians);
 					
 					console.log ("xDest is " + xDest);
 					console.log("ydest is " + yDest);
@@ -409,7 +417,251 @@
 				}
 			}
 			
-		}  
-	} //END ON FRAME FUNCTION
+			if (x < eventList.length) {
+			if (eventList[x].command == "backward") {
+				console.log("SHOULD GO BACKWARD");
+				
+				
+				
+				//calculate distancex and distancey if not calculated yet
+				if (!eventList[x].hasOwnProperty("distancex")) {
+					
+					//set xdistance
+					if (eventList[x].xend > xcoord ) eventList[x].distancex = eventList[x].xend - xcoord;
+					 else eventList[x].distancex = xcoord - eventList[x].xend;
+					console.log("x distance is " + eventList[x].distancex);
+					//setydistance
+					//maybe try putting prevous x-1 event's xend yend
+					 if (eventList[x].yend >ycoord ) eventList[x].distancey = eventList[x].yend - ycoord;
+					 else eventList[x].distancey = ycoord - eventList[x].yend;
+					console.log("y distance is " + eventList[x].distancey);
+					
+					
+					//check which direction to go in (see above comment)
+					if (eventList[x].xend < xcoord) {
+						eventList[x].distancex = -eventList[x].distancex;
+					}
+					if (eventList[x].yend > ycoord) {
+						eventList[x].distancey = -eventList[x].distancey;
+					}
+				}
+				
+				//calculate hypotenuse to get distance required if not calculated yet
+				if (!eventList[x].hasOwnProperty("distanceh")) {
+					eventList[x].distanceh = Math.sqrt((eventList[x].distancex * eventList[x].distancex) + (eventList[x].distancey * eventList[x].distancey))
+				}
+				
+				console.log("distanceh is " + eventList[x].distanceh);
+				
+				var difx;
+				difx = xcoord - eventList[x].xend;
+				difx = Math.abs(difx);
+				var dify;
+				dify = ycoord - eventList[x].yend;
+				dify = Math.abs(dify);
+				
+				//check if done
+				if ((Math.floor(difx) == 0) && (Math.floor(dify) == 0 )){
+					x++;
+				
+				} else if (difx < speed && dify < speed) {
+					//go to end location
+					console.log("Current x is: " + xcoord + " endx is " + eventList[x].xend);
+					console.log("Current y is: " + ycoord + " endy is " + eventList[x].yend);
+					
+					
+					var newx = Math.floor( eventList[x].xend);
+					var newy = Math.floor( eventList[x].yend);
+					
+					//draw the turtle
+					//fix turtle path stuff
+					// whe have eventList[x].xend it goes there
+					turtlePath.add(new paper.Point(newx, newy));
 
+					//move turtle to new point
+					raster.position.x = eventList[x].xend;
+					raster.position.y = eventList[x].yend;
+
+					ycoord = eventList[x].yend;
+					xcoord = eventList[x].xend;
+						
+					console.log ("Now x is " + xcoord + " Now y is " + ycoord);
+					++x;
+				} else {
+					//reverse angle temporarily
+					turtleAngle = -turtleAngle;
+					//convert to radians
+					var angleRadians = turtleAngle * (Math.PI / 180);
+									
+					//calculate coordinate 
+					yDest = Math.sin(angleRadians);
+					yDest = yDest; //y coord increase in down direction 
+					xDest = -Math.cos(angleRadians);
+					
+					console.log ("xDest is " + xDest);
+					console.log("ydest is " + yDest);
+					
+					if (speed <= eventList[x].distanceh) {
+						xDest = xDest * speed;
+						yDest = yDest * speed;
+
+						//updsate distanceh (appropriately...)
+						eventList[x].distanceh = Math.sqrt(((eventList[x].xend-xcoord) * (eventList[x].xend-xcoord)) + ((eventList[x].yend-ycoord) * (eventList[x].yend-ycoord)))
+
+						// increase path
+						turtlePath.add(new paper.Point (xcoord - xDest, ycoord - yDest)); 
+
+						//move turtle to new point
+						raster.position.x = xcoord - xDest;
+						raster.position.y = ycoord - yDest;
+
+						//update new coordinate for turtle position
+						xcoord = xcoord - xDest;
+						ycoord = ycoord - yDest;
+					}else {
+						console.log("should see me once!!");
+						//go to end location
+
+						turtlePath.add(new paper.Point (xcoord, ycoord)); 
+
+						//move turtle to new point
+						raster.position.x = xcoord;
+						raster.position.y = ycoord;
+
+
+						ycoord = eventList[x].yend;
+						xcoord = eventList[x].xend;
+						++x;
+					}
+					//set angle back
+					turtleAngle = -turtleAngle;
+				}
+						
+			}
+			}
+			
+			if (x < eventList.length) {
+			if (eventList[x].command == "goTo") {
+				newAngle = eventList[x].angle;
+				oldAngle = turtleAngle;
+				turtleAngle = newAngle;
+				
+				//calculate distancex and distancey if not calculated yet
+				if (!eventList[x].hasOwnProperty("distancex")) {
+					
+					//set xdistance
+					if (eventList[x].xend > xcoord ) eventList[x].distancex = eventList[x].xend - xcoord;
+					 else eventList[x].distancex = xcoord - eventList[x].xend;
+					console.log("x distance is " + eventList[x].distancex);
+					//setydistance
+					//maybe try putting prevous x-1 event's xend yend
+					 if (eventList[x].yend >ycoord ) eventList[x].distancey = eventList[x].yend - ycoord;
+					 else eventList[x].distancey = ycoord - eventList[x].yend;
+					console.log("y distance is " + eventList[x].distancey);
+					
+					//check which direction to go in (see above comment)
+					if (eventList[x].xend < xcoord) {
+						eventList[x].distancex = -eventList[x].distancex;
+					}
+					if (eventList[x].yend > ycoord) {
+						eventList[x].distancey = -eventList[x].distancey;
+					}
+				}
+				
+				//calculate hypotenuse to get distance required if not calculated yet
+				if (!eventList[x].hasOwnProperty("distanceh")) {
+					eventList[x].distanceh = Math.sqrt((eventList[x].distancex * eventList[x].distancex) + (eventList[x].distancey * eventList[x].distancey))
+				}
+				
+				console.log("distanceh is " + eventList[x].distanceh);
+				
+				var difx;
+				difx = xcoord - eventList[x].xend;
+				difx = Math.abs(difx);
+				var dify;
+				dify = ycoord - eventList[x].yend;
+				dify = Math.abs(dify);
+				
+				//check if done
+				if ((Math.floor(difx) == 0) && (Math.floor(dify) == 0 )){
+					x++;
+					turtleAngle = oldAngle;
+				
+				} else if (difx < speed && dify < speed) {
+					//go to end location
+					console.log("Current x is: " + xcoord + " endx is " + eventList[x].xend);
+					console.log("Current y is: " + ycoord + " endy is " + eventList[x].yend);
+					
+					
+					var newx = Math.floor( eventList[x].xend);
+					var newy = Math.floor( eventList[x].yend);
+					
+					//draw the turtle
+					//fix turtle path stuff
+					// whe have eventList[x].xend it goes there
+					turtlePath.add(new paper.Point(newx, newy));
+
+					//move turtle to new point
+					raster.position.x = eventList[x].xend;
+					raster.position.y = eventList[x].yend;
+
+					ycoord = eventList[x].yend;
+					xcoord = eventList[x].xend;
+						
+					console.log ("Now x is " + xcoord + " Now y is " + ycoord);
+					++x;
+				} else {
+					//reverse angle temporarily
+					turtleAngle = -turtleAngle;
+					//convert to radians
+					var angleRadians = turtleAngle * (Math.PI / 180);
+									
+					//calculate coordinate 
+					yDest = Math.sin(angleRadians);
+					yDest = yDest; //y coord increase in down direction 
+					xDest = -Math.cos(angleRadians);
+					
+					console.log ("xDest is " + xDest);
+					console.log("ydest is " + yDest);
+					
+					if (speed <= eventList[x].distanceh) {
+						xDest = xDest * speed;
+						yDest = yDest * speed;
+
+						//updsate distanceh (appropriately...)
+						eventList[x].distanceh = Math.sqrt(((eventList[x].xend-xcoord) * (eventList[x].xend-xcoord)) + ((eventList[x].yend-ycoord) * (eventList[x].yend-ycoord)))
+
+						// increase path
+						turtlePath.add(new paper.Point (xcoord - xDest, ycoord - yDest)); 
+
+						//move turtle to new point
+						raster.position.x = xcoord - xDest;
+						raster.position.y = ycoord - yDest;
+
+						//update new coordinate for turtle position
+						xcoord = xcoord - xDest;
+						ycoord = ycoord - yDest;
+					}else {
+						console.log("should see me once!!");
+						//go to end location
+
+						turtlePath.add(new paper.Point (xcoord, ycoord)); 
+
+						//move turtle to new point
+						raster.position.x = xcoord;
+						raster.position.y = ycoord;
+
+
+						ycoord = eventList[x].yend;
+						xcoord = eventList[x].xend;
+						++x;
+					}
+					//set angle back
+					turtleAngle = -turtleAngle;
+				}
+						
+			}
+			}
+		}  
+	} 
 	
